7f3a3d8155b9c0722c412ed12e0d59eef2639b5d
leorld <leorld@naver.com>
2020-08-02 20:48:37 +0900

    양식변경, 암호화토큰추가, 회원가입정보 함수작성, db저장

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from .models import Fcuser
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def register(request):  # 함수를 url에 연결하면 request변수를 통해서 들어옴
     # template폴더를 이미 바라보고있기 때문에 그 안의 register.html로 바로 접근가능
-    return render(request, 'register.html')
+    if request.method == "GET":
+        return render(request, 'register.html')
+    elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        re_password = request.POST['re-password']
+
+        if password != re_password:
+            return HttpResponse('비밀번호가 다릅니다!')
+
+        fcuser = Fcuser(
+            username=username,
+            password=password
+        )
+        # 입력받은 값으로 객체를 생성하고
+        fcuser.save()
+        # 생성한 객체를 데이터베이스에 저장
+
+        return render(request, 'register.html')
