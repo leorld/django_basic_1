c1e754319f78b619d2be7fb2285cb39c6efd1b25
leorld <leorld@naver.com>
2020-08-03 22:24:33 +0900

    게시판 글쓰기, 상세 페이지 확인

--- fc_community/board/urls.py
+++ fc_community/board/urls.py
@@ -4,4 +4,7 @@ from . import views  # views.py 를 import
 urlpatterns = [
     # views내의 register함수를 호출 -> register.html페이지 호출
     path('list/', views.board_list),
+    path('write/', views.board_write),
+    path('detail/<int:pk>', views.board_detail),
+    # 숫자가 pk 변수값으로 int형태로 들어오면서 views의 board_detail함수에서 받음
 ]
